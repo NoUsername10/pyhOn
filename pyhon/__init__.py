ca1cf7538a55c6ab21805afedf2fd27a88401b52
NoUsername10 <71009757+NoUsername10@users.noreply.github.com>
2025-05-18 18:11:58 +0200

    Update __init__.py

--- pyhon/__init__.py
+++ pyhon/__init__.py
@@ -2,6 +2,6 @@ from .connection.api import HonAPI
 from .hon import Hon
 import logging
 _LOGGER = logging.getLogger(__name__)
-_LOGGER.debug("Loaded pyhOn from NoUsername10's fork!")
+_LOGGER.warning("Loaded pyhOn from NoUsername10's fork!")
 
 __all__ = ["Hon", "HonAPI"]
